7e2f5bf1dda30831d7549f04f47849ef87831540
Paul Tagliamonte <tag@pault.ag>
2015-09-14 13:51:50 -0400

    Add in audit script to figure out totals.

---
+++ audit.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2015, Paul R. Tagliamonte <paultag@opensource.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import sys
+
+from collections import defaultdict
+
+
+def audit_identifiers(licenses):
+    schemes = defaultdict(lambda: 0)
+
+    for license in licenses:
+        for identifier in license.get('identifiers', []):
+            schemes[identifier['scheme']] += 1
+
+    total = len(licenses)
+
+    return [
+        {"scheme": scheme,
+         "count": count,
+         "percent": count/total,} for (scheme, count) in schemes.items()
+    ]
+
+
+def audit(path):
+    with open(path, 'r') as fd:
+        licenses = json.load(fd)
+    report = {"identifiers": audit_identifiers(licenses)}
+    json.dump(report, sys.stdout, indent=4, sort_keys=True)
+
+
+audit(*sys.argv[1:])
